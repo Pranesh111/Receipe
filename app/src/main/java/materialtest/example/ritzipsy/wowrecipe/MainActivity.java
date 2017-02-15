ef5abf2b958f653497d3c5a38b6b9c7a0c43d711
Pranesh <Pranesh.msc@gmail.com>
2017-02-15 21:19:42 +0530

    My Receipe First commit

---
+++ app/src/main/java/materialtest/example/ritzipsy/wowrecipe/MainActivity.java
@@ -0,0 +1,13 @@
+package materialtest.example.ritzipsy.wowrecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
